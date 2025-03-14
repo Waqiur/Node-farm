43f9c8c78aa50e30fa16fcc10f06da30ade85e68
Waqiur <Wakiurrahmanansari@gmail.com>
2025-03-14 23:21:17 +0530

    refactor: move replaceTemplate function to a separate module and add .gitignore

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ import url from "url";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import replaceTemplate from "./modules/replaceTemplate.js";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if (!product.organic)
-        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    return output;
-};
 const tempOverview = fs.readFileSync(
     `${__dirname}/templates/template-overview.html`,
     "utf-8"
